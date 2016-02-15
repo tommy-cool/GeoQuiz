5f59f2d5974bd3f159b0fe19004fb39712097cad
tommy-cool <trebers@gmail.com>
2016-02-14 22:21:42 -0600

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by Thomas on 2/11/2016.
+ */
+public class Question {
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
